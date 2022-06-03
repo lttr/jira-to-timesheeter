414beee502a259dae3eded333c5c1d6f0c549e20
Lukas Trumm <lukas.trumm@gmail.com>
2022-06-03 07:02:06 +0200

    Cache implemented as a function

--- ui/cache.js
+++ ui/cache.js
@@ -1,45 +1,17 @@
-class DataCache {
-  constructor(fetchFunction, millisecondsToLive) {
-    this.millisecondsToLive = millisecondsToLive;
-    this.fetchFunction = fetchFunction;
-    this.cache = null;
-    this.getData = this.getData.bind(this);
-    this.isCacheExpired = this.isCacheExpired.bind(this);
-    this.fetchDate = new Date(0);
-  }
-  isCacheExpired() {
-    return (
-      this.fetchDate.getTime() + this.millisecondsToLive < new Date().getTime()
-    );
-  }
-  async getData() {
-    if (!this.cache || this.isCacheExpired()) {
-      console.log("expired - fetching new data");
-      const data = await this.fetchFunction();
-      this.cache = data;
-      this.fetchDate = new Date();
+export function createCache(fetchFunction, ttl) {
+  let cache = null;
+  let fetchDate = new Date();
+
+  const isCacheExpired = () => fetchDate.getTime() + ttl < new Date().getTime();
+
+  return async () => {
+    if (!cache || isCacheExpired()) {
+      const data = await fetchFunction();
+      cache = data;
+      fetchDate = new Date();
       return data;
     } else {
-      console.log("cache hit");
-      return this.cache;
+      return cache;
     }
-  }
-}
-
-async function getData() {
-  const asyncData = () => {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve("abc"), 100);
-    });
-  };
-  const data = await asyncData();
-  console.log("getData called with ", data);
-  return {
-    foo: data,
   };
 }
-const cache = new DataCache(getData, 1000);
-
-setTimeout(() => cache.getData(), 500);
-setTimeout(() => cache.getData(), 1500);
-setTimeout(() => cache.getData(), 2500);
