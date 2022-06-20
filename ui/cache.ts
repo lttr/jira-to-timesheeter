8f3ea318bc315fe19e4568a8fd37e532771b070d
Lukas Trumm <lukas.trumm@gmail.com>
2022-06-20 13:55:18 +0200

    Improve cache

--- ui/cache.ts
+++ ui/cache.ts
@@ -6,26 +6,27 @@ export function createCache<T>(
   fetchFunction: () => Promise<T>,
   ttl: number
 ): Cache<T> {
-  const cache: Record<string, T | null> = {};
-  let fetchDate = new Date();
-
-  const isCacheExpired = () => fetchDate.getTime() + ttl < new Date().getTime();
+  const cache: Record<string, { date: Date; value: T | null }> = {};
+  const isCacheExpired = (date: Date) =>
+    date.getTime() + ttl < new Date().getTime();
 
   return {
     async getData(key: string) {
-      if (!cache[key] || isCacheExpired()) {
+      if (!cache[key] || isCacheExpired(cache[key].date)) {
         let data = null;
         try {
           data = await fetchFunction();
-          cache[key] = data;
-          fetchDate = new Date();
+          cache[key] = {
+            value: data,
+            date: new Date(),
+          };
         } catch (e) {
           console.warn(e);
         }
         return data;
       } else {
-        console.debug("Cache hit");
-        return cache[key];
+        console.debug("Cache hit for key: ", key);
+        return cache[key].value;
       }
     },
   };
